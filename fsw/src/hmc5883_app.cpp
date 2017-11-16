5d36d8d6fb6b37c543ec9f49bd6cb4c82651506f
Mathew Benson <mbenson@windhoverlabs.com>
2017-11-16 16:54:41 +0000

    Fixed template to process command correctly.  Added skeletal code for hmc5883 driver, land detector, and rc input.

---
+++ fsw/src/hmc5883_app.cpp
@@ -0,0 +1,489 @@
+/************************************************************************
+** Includes
+*************************************************************************/
+#include <string.h>
+
+#include "cfe.h"
+
+#include "hmc5883_app.h"
+#include "hmc5883_msg.h"
+#include "hmc5883_version.h"
+
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/*                                                                 */
+/* Instantiate the application object.                             */
+/*                                                                 */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+HMC5883 oHMC5883;
+
+
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/*                                                                 */
+/* Default constructor.                                            */
+/*                                                                 */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+HMC5883::HMC5883()
+{
+
+}
+
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/*                                                                 */
+/* Destructor constructor.                                         */
+/*                                                                 */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+HMC5883::~HMC5883()
+{
+
+}
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/*                                                                 */
+/* Initialize event tables.                                        */
+/*                                                                 */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+int32 HMC5883::InitEvent()
+{
+    int32  iStatus=CFE_SUCCESS;
+
+    /* Register the table with CFE */
+    iStatus = CFE_EVS_Register(0, 0, CFE_EVS_BINARY_FILTER);
+    if (iStatus != CFE_SUCCESS)
+    {
+        (void) CFE_ES_WriteToSysLog("HMC5883 - Failed to register with EVS (0x%08lX)\n", iStatus);
+    }
+
+    return iStatus;
+}
+
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/*                                                                 */
+/* Initialize Message Pipes                                        */
+/*                                                                 */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+int32 HMC5883::InitPipe()
+{
+    int32  iStatus=CFE_SUCCESS;
+
+    /* Init schedule pipe and subscribe to wakeup messages */
+    iStatus = CFE_SB_CreatePipe(&SchPipeId,
+    		HMC5883_SCH_PIPE_DEPTH,
+			HMC5883_SCH_PIPE_NAME);
+    if (iStatus == CFE_SUCCESS)
+    {
+        iStatus = CFE_SB_SubscribeEx(HMC5883_WAKEUP_MID, SchPipeId, CFE_SB_Default_Qos, HMC5883_WAKEUP_MID_MAX_MSG_COUNT);
+        if (iStatus != CFE_SUCCESS)
+        {
+            (void) CFE_EVS_SendEvent(HMC5883_SUBSCRIBE_ERR_EID, CFE_EVS_ERROR,
+            		"Sch Pipe failed to subscribe to HMC5883_WAKEUP_MID. (0x%08lX)",
+                    iStatus);
+            goto HMC5883_InitPipe_Exit_Tag;
+        }
+
+        iStatus = CFE_SB_SubscribeEx(HMC5883_SEND_HK_MID, SchPipeId, CFE_SB_Default_Qos, HMC5883_SEND_HK_MID_MAX_MSG_COUNT);
+        if (iStatus != CFE_SUCCESS)
+        {
+            (void) CFE_EVS_SendEvent(HMC5883_SUBSCRIBE_ERR_EID, CFE_EVS_ERROR,
+					 "CMD Pipe failed to subscribe to HMC5883_SEND_HK_MID. (0x%08X)",
+					 (unsigned int)iStatus);
+            goto HMC5883_InitPipe_Exit_Tag;
+        }
+    }
+    else
+    {
+        (void) CFE_EVS_SendEvent(HMC5883_PIPE_INIT_ERR_EID, CFE_EVS_ERROR,
+			 "Failed to create SCH pipe (0x%08lX)",
+			 iStatus);
+        goto HMC5883_InitPipe_Exit_Tag;
+    }
+
+    /* Init command pipe and subscribe to command messages */
+    iStatus = CFE_SB_CreatePipe(&CmdPipeId,
+    		HMC5883_CMD_PIPE_DEPTH,
+			HMC5883_CMD_PIPE_NAME);
+    if (iStatus == CFE_SUCCESS)
+    {
+        /* Subscribe to command messages */
+        iStatus = CFE_SB_Subscribe(HMC5883_CMD_MID, CmdPipeId);
+
+        if (iStatus != CFE_SUCCESS)
+        {
+            (void) CFE_EVS_SendEvent(HMC5883_SUBSCRIBE_ERR_EID, CFE_EVS_ERROR,
+				 "CMD Pipe failed to subscribe to HMC5883_CMD_MID. (0x%08lX)",
+				 iStatus);
+            goto HMC5883_InitPipe_Exit_Tag;
+        }
+    }
+    else
+    {
+        (void) CFE_EVS_SendEvent(HMC5883_PIPE_INIT_ERR_EID, CFE_EVS_ERROR,
+			 "Failed to create CMD pipe (0x%08lX)",
+			 iStatus);
+        goto HMC5883_InitPipe_Exit_Tag;
+    }
+
+HMC5883_InitPipe_Exit_Tag:
+    return iStatus;
+}
+    
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/*                                                                 */
+/* Initialize Global Variables                                     */
+/*                                                                 */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+void HMC5883::InitData()
+{
+    /* Init housekeeping message. */
+    CFE_SB_InitMsg(&HkTlm,
+    		HMC5883_HK_TLM_MID, sizeof(HkTlm), TRUE);
+      /* Init output messages */
+      CFE_SB_InitMsg(&SensorMagMsg,
+      		PX4_SENSOR_MAG_MID, sizeof(PX4_SensorMagMsg_t), TRUE);
+}
+
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/*                                                                 */
+/* HMC5883 initialization                                              */
+/*                                                                 */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+int32 HMC5883::InitApp()
+{
+    int32  iStatus   = CFE_SUCCESS;
+    int8   hasEvents = 0;
+
+    iStatus = InitEvent();
+    if (iStatus != CFE_SUCCESS)
+    {
+        (void) CFE_ES_WriteToSysLog("HMC5883 - Failed to init events (0x%08lX)\n", iStatus);
+        goto HMC5883_InitApp_Exit_Tag;
+    }
+    else
+    {
+        hasEvents = 1;
+    }
+
+    iStatus = InitPipe();
+    if (iStatus != CFE_SUCCESS)
+    {
+        goto HMC5883_InitApp_Exit_Tag;
+    }
+
+    InitData();
+
+    iStatus = InitConfigTbl();
+    if (iStatus != CFE_SUCCESS)
+    {
+        goto HMC5883_InitApp_Exit_Tag;
+    }
+
+HMC5883_InitApp_Exit_Tag:
+    if (iStatus == CFE_SUCCESS)
+    {
+        (void) CFE_EVS_SendEvent(HMC5883_INIT_INF_EID, CFE_EVS_INFORMATION,
+                                 "Initialized.  Version %d.%d.%d.%d",
+								 HMC5883_MAJOR_VERSION,
+								 HMC5883_MINOR_VERSION,
+								 HMC5883_REVISION,
+								 HMC5883_MISSION_REV);
+    }
+    else
+    {
+        if (hasEvents == 1)
+        {
+            (void) CFE_ES_WriteToSysLog("HMC5883 - Application failed to initialize\n");
+        }
+    }
+
+    return iStatus;
+}
+
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/*                                                                 */
+/* Receive and Process Messages                                    */
+/*                                                                 */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+int32 HMC5883::RcvSchPipeMsg(int32 iBlocking)
+{
+    int32           iStatus=CFE_SUCCESS;
+    CFE_SB_Msg_t*   MsgPtr=NULL;
+    CFE_SB_MsgId_t  MsgId;
+
+    /* Stop Performance Log entry */
+    CFE_ES_PerfLogExit(HMC5883_MAIN_TASK_PERF_ID);
+
+    /* Wait for WakeUp messages from scheduler */
+    iStatus = CFE_SB_RcvMsg(&MsgPtr, SchPipeId, iBlocking);
+
+    /* Start Performance Log entry */
+    CFE_ES_PerfLogEntry(HMC5883_MAIN_TASK_PERF_ID);
+
+    if (iStatus == CFE_SUCCESS)
+    {
+        MsgId = CFE_SB_GetMsgId(MsgPtr);
+        switch (MsgId)
+        {
+            case HMC5883_WAKEUP_MID:
+                /* TODO:  Do something here. */
+                break;
+
+            case HMC5883_SEND_HK_MID:
+                ProcessCmdPipe();
+                ReportHousekeeping();
+                break;
+
+            default:
+                (void) CFE_EVS_SendEvent(HMC5883_MSGID_ERR_EID, CFE_EVS_ERROR,
+                     "Recvd invalid SCH msgId (0x%04X)", MsgId);
+        }
+    }
+    else if (iStatus == CFE_SB_NO_MESSAGE)
+    {
+        /* TODO: If there's no incoming message, you can do something here, or 
+         * nothing.  Note, this section is dead code only if the iBlocking arg
+         * is CFE_SB_PEND_FOREVER. */
+        iStatus = CFE_SUCCESS;
+    }
+    else if (iStatus == CFE_SB_TIME_OUT)
+    {
+        /* TODO: If there's no incoming message within a specified time (via the
+         * iBlocking arg, you can do something here, or nothing.  
+         * Note, this section is dead code only if the iBlocking arg
+         * is CFE_SB_PEND_FOREVER. */
+        iStatus = CFE_SUCCESS;
+    }
+    else
+    {
+        (void) CFE_EVS_SendEvent(HMC5883_RCVMSG_ERR_EID, CFE_EVS_ERROR,
+			  "SCH pipe read error (0x%08lX).", iStatus);
+    }
+
+    return iStatus;
+}
+
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/*                                                                 */
+/* Process Incoming Commands                                       */
+/*                                                                 */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+void HMC5883::ProcessCmdPipe()
+{
+    int32 iStatus = CFE_SUCCESS;
+    CFE_SB_Msg_t*   CmdMsgPtr=NULL;
+    CFE_SB_MsgId_t  CmdMsgId;
+
+    /* Process command messages until the pipe is empty */
+    while (1)
+    {
+        iStatus = CFE_SB_RcvMsg(&CmdMsgPtr, CmdPipeId, CFE_SB_POLL);
+        if(iStatus == CFE_SUCCESS)
+        {
+            CmdMsgId = CFE_SB_GetMsgId(CmdMsgPtr);
+            switch (CmdMsgId)
+            {
+                case HMC5883_CMD_MID:
+                    ProcessAppCmds(CmdMsgPtr);
+                    break;
+
+                default:
+                    /* Bump the command error counter for an unknown command.
+                     * (This should only occur if it was subscribed to with this
+                     *  pipe, but not handled in this switch-case.) */
+                    HkTlm.usCmdErrCnt++;
+                    (void) CFE_EVS_SendEvent(HMC5883_MSGID_ERR_EID, CFE_EVS_ERROR,
+                                      "Recvd invalid CMD msgId (0x%04X)", (unsigned short)CmdMsgId);
+                    break;
+            }
+        }
+        else if (iStatus == CFE_SB_NO_MESSAGE)
+        {
+            break;
+        }
+        else
+        {
+            (void) CFE_EVS_SendEvent(HMC5883_RCVMSG_ERR_EID, CFE_EVS_ERROR,
+                  "CMD pipe read error (0x%08lX)", iStatus);
+            break;
+        }
+    }
+}
+
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/*                                                                 */
+/* Process HMC5883 Commands                                            */
+/*                                                                 */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+void HMC5883::ProcessAppCmds(CFE_SB_Msg_t* MsgPtr)
+{
+    uint32  uiCmdCode=0;
+
+    if (MsgPtr != NULL)
+    {
+        uiCmdCode = CFE_SB_GetCmdCode(MsgPtr);
+        switch (uiCmdCode)
+        {
+            case HMC5883_NOOP_CC:
+                HkTlm.usCmdCnt++;
+                (void) CFE_EVS_SendEvent(HMC5883_CMD_NOOP_EID, CFE_EVS_INFORMATION,
+					"Recvd NOOP. Version %d.%d.%d.%d",
+					HMC5883_MAJOR_VERSION,
+					HMC5883_MINOR_VERSION,
+					HMC5883_REVISION,
+					HMC5883_MISSION_REV);
+                break;
+
+            case HMC5883_RESET_CC:
+                HkTlm.usCmdCnt = 0;
+                HkTlm.usCmdErrCnt = 0;
+                break;
+
+            default:
+                HkTlm.usCmdErrCnt++;
+                (void) CFE_EVS_SendEvent(HMC5883_CC_ERR_EID, CFE_EVS_ERROR,
+                                  "Recvd invalid command code (%u)", (unsigned int)uiCmdCode);
+                break;
+        }
+    }
+}
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/*                                                                 */
+/* Send HMC5883 Housekeeping                                           */
+/*                                                                 */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+void HMC5883::ReportHousekeeping()
+{
+    CFE_SB_TimeStampMsg((CFE_SB_Msg_t*)&HkTlm);
+    CFE_SB_SendMsg((CFE_SB_Msg_t*)&HkTlm);
+}
+
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/*                                                                 */
+/* Publish Output Data                                             */
+/*                                                                 */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+void HMC5883::SendSensorMagMsg()
+{
+    CFE_SB_TimeStampMsg((CFE_SB_Msg_t*)&SensorMagMsg);
+    CFE_SB_SendMsg((CFE_SB_Msg_t*)&SensorMagMsg);
+}
+
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/*                                                                 */
+/* Verify Command Length                                           */
+/*                                                                 */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+boolean HMC5883::VerifyCmdLength(CFE_SB_Msg_t* MsgPtr,
+                           uint16 usExpectedLen)
+{
+    boolean bResult  = TRUE;
+    uint16  usMsgLen = 0;
+
+    if (MsgPtr != NULL)
+    {
+        usMsgLen = CFE_SB_GetTotalMsgLength(MsgPtr);
+
+        if (usExpectedLen != usMsgLen)
+        {
+            bResult = FALSE;
+            CFE_SB_MsgId_t MsgId = CFE_SB_GetMsgId(MsgPtr);
+            uint16 usCmdCode = CFE_SB_GetCmdCode(MsgPtr);
+
+            (void) CFE_EVS_SendEvent(HMC5883_MSGLEN_ERR_EID, CFE_EVS_ERROR,
+                              "Rcvd invalid msgLen: msgId=0x%08X, cmdCode=%d, "
+                              "msgLen=%d, expectedLen=%d",
+                              MsgId, usCmdCode, usMsgLen, usExpectedLen);
+            HkTlm.usCmdErrCnt++;
+        }
+    }
+
+    return bResult;
+}
+
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/*                                                                 */
+/* HMC5883 Application C style main entry point.                       */
+/*                                                                 */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+extern "C" void HMC5883_AppMain()
+{
+    oHMC5883.AppMain();
+}
+
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/*                                                                 */
+/* HMC5883 Application C++ style main entry point.                     */
+/*                                                                 */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+void HMC5883::AppMain()
+{
+    /* Register the application with Executive Services */
+    uiRunStatus = CFE_ES_APP_RUN;
+
+    int32 iStatus = CFE_ES_RegisterApp();
+    if (iStatus != CFE_SUCCESS)
+    {
+        (void) CFE_ES_WriteToSysLog("HMC5883 - Failed to register the app (0x%08lX)\n", iStatus);
+    }
+
+    /* Start Performance Log entry */
+    CFE_ES_PerfLogEntry(HMC5883_MAIN_TASK_PERF_ID);
+
+    /* Perform application initializations */
+    if (iStatus == CFE_SUCCESS)
+    {
+        iStatus = InitApp();
+    }
+
+    if (iStatus == CFE_SUCCESS)
+    {
+        /* Do not perform performance monitoring on startup sync */
+        CFE_ES_PerfLogExit(HMC5883_MAIN_TASK_PERF_ID);
+        CFE_ES_WaitForStartupSync(HMC5883_STARTUP_TIMEOUT_MSEC);
+        CFE_ES_PerfLogEntry(HMC5883_MAIN_TASK_PERF_ID);
+    }
+    else
+    {
+        uiRunStatus = CFE_ES_APP_ERROR;
+    }
+
+    /* Application main loop */
+    while (CFE_ES_RunLoop(&uiRunStatus) == TRUE)
+    {
+        RcvSchPipeMsg(HMC5883_SCH_PIPE_PEND_TIME);
+
+        iStatus = AcquireConfigPointers();
+        if(iStatus != CFE_SUCCESS)
+        {
+            /* We apparently tried to load a new table but failed.  Terminate the application. */
+            uiRunStatus = CFE_ES_APP_ERROR;
+        }
+    }
+
+    /* Stop Performance Log entry */
+    CFE_ES_PerfLogExit(HMC5883_MAIN_TASK_PERF_ID);
+
+    /* Exit the application */
+    CFE_ES_ExitApp(uiRunStatus);
+}
+
+
+/************************/
+/*  End of File Comment */
+/************************/
